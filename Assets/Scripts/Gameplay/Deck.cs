71d8304b498bfadd185e011c035b3d5b7de08a24
Gavin <gavin-farrell@hotmail.com>
2022-11-04 20:37:44 -0700

    Added deck script
    + Deck class
    + Card class
    + Dealer class

--- Assets/Scripts/Gameplay/Deck.cs
+++ Assets/Scripts/Gameplay/Deck.cs
@@ -1,143 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
 using UnityEngine;
 
-public class Deck : MonoBehaviour
+public class Deck
 {
-    // Start is called before the first frame update
+    private List<Card> _allCards = new List<Card>();
+    private List<Card> _remainingCards = new List<Card>();
 
-    string[] minorArcanaCardSuits = new string[] { "Pentacles", "Swords", "Wands", "Cups"};
-    string[] minorArcanaCardRanks = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "1"};
-    string[] majorArcana = new string[] { "0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI" };
-    
-    //needs to be changed to list for game in order to allow shuffling of remaining cards.
-
-    public List<string> deck;
-
-    public List<string> discardPile;
-
-    //public int deckLocation;
-
-    
-    void Start()
+    public Deck()
     {
-        generateDeck();
-        //generate deck on start
-/*        deck = new string[78]; 
-        deckLocation = 0;
-        for(int i = 0; i < minorArcanaCardSuits.Length; i++)
-        {
+        for (int suit = 0; suit < 4; suit++)
+            for (int value = 1; value <= 14; value++)
+                _allCards.Add(new Card((Card.Suit) suit, value));
 
-            for(int j = 0; j < minorArcanaCardRanks.Length; j++)
-            {
-                deck[deckLocation] = minorArcanaCardRanks[j] + "Of" + minorArcanaCardSuits[i];
-                deckLocation++;
-          }
-      }
-        for (int i = 0; i < majorArcana.Length; i++)
-        {
-            deck[deckLocation] = majorArcana[i];
-            deckLocation++;
-        }
-*/
-        //reset deck location to 0
-//        deckLocation = 0;
-
-        //shuffle the deck
-        shuffle();
+        _remainingCards.AddRange(_allCards);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public Card DrawRandomCard(bool removeFromDeck)
     {
-        //Debug code - to be transitioned for prod
+        int randomCardIndex = Random.Range(0, _remainingCards.Count);
+        Card card = _remainingCards[randomCardIndex];
 
-        //show deck - t
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            for (int i = 0; i < deck.Count; i++)
-            {
-                Debug.Log(deck[i]);
-            }
-            
-        }
-        //shuffle - s
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            shuffle();
-            Debug.Log(deck);
-        }
-        //deal - d
+        if (removeFromDeck)
+            _remainingCards.Remove(card);
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            string[] cards = deal(5);
-
-            Debug.Log("Example Deal");
-
-            for(int i = 0; i < cards.Length; i++)
-            {
-                Debug.Log(cards[i]);
-            }
-        }
-    }
-
-    //shuffle the deck
-    public void shuffle()
-    {
-        for(int i = 0; i < deck.Count; i++)
-        {
-            int x = (Random.Range(0, deck.Count - 1));
-            string temp;
-            temp = deck[x];
-            deck[x] = deck[i];
-            deck[i] = temp;
-            
-        }
-    }
-
-  //static void swapnum(string x, string y){    }
-    
-    //deal a certain number of cards
-    public string[] deal(int numToDeal)
-    { 
-        string[] dealtHand = new string[numToDeal];
-        
-        //for(int i = 0; i < numToDeal; i++)
-        
-        dealtHand = deck.GetRange(0, numToDeal).ToArray();
- //           deckLocation++;
-        deck.RemoveRange(0, numToDeal);
-
-        return dealtHand;
-    }
-
-    public void generateDeck()
-    {
-        //DEPRECIATED
-        //___________
-
-        //deck = new string[78];
-        //deckLocation = 0;
-        //___________
-
-        for (int i = 0; i < minorArcanaCardSuits.Length; i++)
-        {
-
-            for (int j = 0; j < minorArcanaCardRanks.Length; j++)
-            {
-                deck.Add(minorArcanaCardRanks[j] + "Of" + minorArcanaCardSuits[i]);
-                //deckLocation++;
-            }
-        }
-        for (int i = 0; i < majorArcana.Length; i++)
-        {
-            deck.Add(majorArcana[i]);
-            //deckLocation++;
-        }
-        //reset deck location to 0
-        //deckLocation = 0;
+        return card;
     }
 }
