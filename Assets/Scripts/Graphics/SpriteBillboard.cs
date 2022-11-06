7b21999da4aa29a707a9c03a2cfb0b05b6beba07
mickeykerr <58183089+mickeykerr@users.noreply.github.com>
2022-11-05 17:08:05 -0700

    Reorganization + Add 2D sprite support
    Watched some videos and was able to get the sprites to constantly look at the player. Did small modification to shader script and was able to allow sprites to get affected by light. Sprites also cast shadows, just add the SpriteShadowEnable script to a sprite and it should work.

---
+++ Assets/Scripts/Graphics/SpriteBillboard.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SpriteBillboard : MonoBehaviour
+{
+
+    
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+    }
+}
