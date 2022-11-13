adfbd5e4da2e0daa46c071682623df2d7a7ff703
JordanNewkirk <jordannewkirk@gmail.com>
2022-11-12 19:40:39 -0800

    Added more npcs and their virtual cameras set up
    camera works with up to 4 npcs now

--- Assets/Scripts/Camera/CameraSwitcher.cs
+++ Assets/Scripts/Camera/CameraSwitcher.cs
@@ -11,6 +11,9 @@ public class CameraSwitcher : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera right;
     [SerializeField] CinemachineVirtualCamera down;
     [SerializeField] CinemachineVirtualCamera NpcCamera;
+    [SerializeField] CinemachineVirtualCamera NpcCamera1;
+    [SerializeField] CinemachineVirtualCamera NpcCamera2;
+    [SerializeField] CinemachineVirtualCamera NpcCamera3;
     [SerializeField] CinemachineVirtualCamera dealerCamera;
 
     Ray ray;
@@ -37,6 +40,7 @@ public class CameraSwitcher : MonoBehaviour
             Debug.Log(hit.collider.name); //running debug to see all object that the mouse hovers over
 
             //Setting up zoom on different objects in the world
+            //Dealer Zoom
             if(hit.collider.name == "Dealer" && Input.GetMouseButtonDown(0))
             {
                 //dealerCameraActive = true;
@@ -47,7 +51,7 @@ public class CameraSwitcher : MonoBehaviour
                 //dealerCameraActive = false;
                 SwitchPriority("m");
             }
-
+            //Npc Zoom
             if(hit.collider.name == "NpcSprite" && Input.GetMouseButtonDown(0))
             {
                 SwitchPriority("npc");
@@ -55,6 +59,33 @@ public class CameraSwitcher : MonoBehaviour
             else if(hit.collider.name == "NpcSprite" && Input.GetMouseButtonDown(1))
             {
                 SwitchPriority("l");
+            }
+            //Npc1 Zoom
+            if (hit.collider.name == "NpcSprite1" && Input.GetMouseButtonDown(0))
+            {
+                SwitchPriority("npc1");
+            }
+            else if (hit.collider.name == "NpcSprite1" && Input.GetMouseButtonDown(1))
+            {
+                SwitchPriority("m");
+            }
+            //Npc2 Zoom
+            if (hit.collider.name == "NpcSprite2" && Input.GetMouseButtonDown(0))
+            {
+                SwitchPriority("npc2");
+            }
+            else if (hit.collider.name == "NpcSprite2" && Input.GetMouseButtonDown(1))
+            {
+                SwitchPriority("m");
+            }
+            //Npc3 Zoom
+            if (hit.collider.name == "NpcSprite3" && Input.GetMouseButtonDown(0))
+            {
+                SwitchPriority("npc3");
+            }
+            else if (hit.collider.name == "NpcSprite3" && Input.GetMouseButtonDown(1))
+            {
+                SwitchPriority("r");
             }
         }
     }
@@ -70,38 +101,50 @@ public class CameraSwitcher : MonoBehaviour
         if(cameraToChange == "l")
         {
             main.Priority = 0;
+            left.Priority = 1;
             right.Priority = 0;
             down.Priority = 0;
             dealerCamera.Priority = 0;
             NpcCamera.Priority = 0;
-            left.Priority = 1;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 0;
         }
         else if(cameraToChange == "r")
         {
             main.Priority = 0;
             left.Priority = 0;
+            right.Priority = 1;
             down.Priority = 0;
             dealerCamera.Priority = 0;
             NpcCamera.Priority = 0;
-            right.Priority = 1;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 0;
         }
         else if(cameraToChange == "d")
         {
             main.Priority = 0;
             left.Priority = 0;
             right.Priority = 0;
+            down.Priority = 1;
             dealerCamera.Priority = 0;
             NpcCamera.Priority = 0;
-            down.Priority = 1;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 0;
         }
         else if(cameraToChange == "m")
         {
+            main.Priority = 1;
             left.Priority = 0;
             right.Priority = 0;
             down.Priority = 0;
             dealerCamera.Priority = 0;
             NpcCamera.Priority = 0;
-            main.Priority = 1;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 0;
         }
 
         else if(cameraToChange == "dealer")
@@ -110,8 +153,11 @@ public class CameraSwitcher : MonoBehaviour
             left.Priority = 0;
             right.Priority = 0;
             down.Priority = 0;
-            NpcCamera.Priority = 0;
             dealerCamera.Priority = 1;
+            NpcCamera.Priority = 0;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 0;
         }
 
         else if(cameraToChange == "npc")
@@ -122,6 +168,51 @@ public class CameraSwitcher : MonoBehaviour
             down.Priority = 0;
             dealerCamera.Priority = 0;
             NpcCamera.Priority = 1;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 0;
+        }
+
+        else if (cameraToChange == "npc1")
+        {
+            main.Priority = 0;
+            left.Priority = 0;
+            right.Priority = 0;
+            down.Priority = 0;
+            dealerCamera.Priority = 0;
+            NpcCamera.Priority = 0;
+            NpcCamera1.Priority = 1;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 0;
+            
+        }
+
+        else if (cameraToChange == "npc2")
+        {
+            main.Priority = 0;
+            left.Priority = 0;
+            right.Priority = 0;
+            down.Priority = 0;
+            dealerCamera.Priority = 0;
+            NpcCamera.Priority = 0;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 1;
+            NpcCamera3.Priority = 0;
+
+        }
+
+        else if (cameraToChange == "npc3")
+        {
+            main.Priority = 0;
+            left.Priority = 0;
+            right.Priority = 0;
+            down.Priority = 0;
+            dealerCamera.Priority = 0;
+            NpcCamera.Priority = 0;
+            NpcCamera1.Priority = 0;
+            NpcCamera2.Priority = 0;
+            NpcCamera3.Priority = 1;
+
         }
     }
 
